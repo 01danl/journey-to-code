601467a9f519b1adb6127066042ff744f3083d98
Daniyal Bissenov <139388744+01danl@users.noreply.github.com>
2024-12-01 23:34:46 +0500

    .

--- Yandex Algorithms/Yandex Algorithm Training 1.0/complexity #1/A.py
+++ Yandex Algorithms/Yandex Algorithm Training 1.0/complexity #1/A.py
@@ -1,1 +1,20 @@
+t_room, t_cond = map(int, input().split())
+res = input()
 
+if res == "freeze":
+    if t_room < t_cond:
+        print(t_room)
+    else:
+        print(t_cond)
+
+if res == "heat":
+    if t_room < t_cond:
+        print(t_cond)
+    else:
+        print(t_room)
+
+if res == "auto":
+    print(t_cond)
+
+if res == "fan":
+    print(t_room)
